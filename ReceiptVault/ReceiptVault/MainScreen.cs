b9a9a81c3160edd24698cc76b2f2ca077a544990
Kirakees <rikhassing_33333@hotmail.com>
2016-05-24 10:13:34 +0200

    Added Mainscreen and NewEntryScreen

---
+++ ReceiptVault/ReceiptVault/MainScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ReceiptVault
+{
+    
+    class MainScreen
+    {
+        List<EntryStore.Entry> entries;
+
+        public MainScreen()
+        {
+            entries = new List<EntryStore.Entry>();
+        }
+
+        void entryClicked()
+        {
+
+        }
+
+        void newRecieptClicked()
+        {
+
+        }
+
+        void VATClicked()
+        {
+
+        }
+
+        void spendingsClicked()
+        {
+            
+        }
+    }
+}
---
+++ ReceiptVault/ReceiptVault/NewEntryScreen.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ReceiptVault
+{
+    class NewEntryScreen
+    {
+        int[,] dragStartPos;
+        int[,] dragFinishPos;
+        EntryStore.Entry entry;
+
+        public NewEntryScreen()
+        {
+
+        }
+
+        public void acceptEntry()
+        {
+
+        }
+
+        public void takePicture()
+        {
+
+        }
+
+        public void dragStart()
+        {
+
+        }
+
+        public void dragFinish()
+        {
+
+        }
+
+        public void dragMove()
+        {
+
+        }
+
+        public void homeClicked()
+        {
+
+        }
+
+        public void spendingsClicked()
+        {
+
+        }
+
+        public void VATClicked()
+        {
+
+        }
+    }
+}
