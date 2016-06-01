9356a59d83cf734dab174371662d83d2d24db27f
JennoVink <jenno-vink@hotmail.com>
2016-06-01 11:58:18 +0200

    De class NewEntryScreen samengevoegd aan de xaml.cs. Je kan foto's maken, je kan die foto's ook weergeven en je kan d.m.v. draggen de begin- en eindcoördinaten bepalen. Deze worden in de output weergegeven. TODO: visuele feedback geven bij het draggen de rest implementeren....

--- ReceiptVault/ReceiptVault/newEntryScreenScreen.xaml.cs
+++ ReceiptVault/ReceiptVault/newEntryScreenScreen.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -9,6 +10,7 @@ using Windows.Graphics.Imaging;
 using Windows.Media.Capture;
 using Windows.Storage;
 using Windows.Storage.Streams;
+using Windows.System.Profile;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -29,56 +31,131 @@ namespace ReceiptVault
     /// </summary>
     public sealed partial class newEntryScreen : Page
     {
+        private int[] dragStartPos;
+        private int[] dragFinishPos;
+        private EntryStore.Entry entry;
+
+        //note: deze staat niet in de class diagram, maar is wel nodig:
+        private Boolean dragging;
+
         public newEntryScreen()
         {
             this.InitializeComponent();
-            testPic.Source = new BitmapImage(new Uri(base.BaseUri, "Assets/testBonnetje.bmp"));
+            Debug.WriteLine("Het werkt");
+
+            dragStartPos = dragFinishPos = new int[2];
         }
 
-        private async void button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// note: deze staat niet in de class diagram
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CompositionTarget_Rendering(object sender, object args)
         {
-            /**   
-               CameraCaptureUI captureUI = new CameraCaptureUI();
-               captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
-               captureUI.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
+            //Used for rendering the cropping rectangle on the image. 
+            //Used for rendering the cropping rectangle on the image. 
+            rect.SetValue(Canvas.LeftProperty, (dragStartPos[0] < dragFinishPos[0]) ? dragStartPos[0] : dragFinishPos[0]);
+            rect.SetValue(Canvas.TopProperty, (dragStartPos[1] < dragFinishPos[1]) ? dragStartPos[1] : dragFinishPos[1]);
+            rect.Width = (int)Math.Abs(dragFinishPos[0] - dragStartPos[0]);
+            rect.Height = (int)Math.Abs(dragFinishPos[1] - dragStartPos[1]);
+        }
 
-               StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+             takePicture();
+        }
 
-               testPic.Source = new BitmapImage(new Uri(base.BaseUri, "Assets/StoreLogo.png"));
-
-               if (photo == null)
-               {
-                   // User cancelled photo capture
-                   System.Diagnostics.Debug.WriteLine("photo = null");
-                   return;
-               }
-
-               IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
-                   BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
-                   SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
-
-                   SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(softwareBitmap,
-                   BitmapPixelFormat.Bgra8,
-                   BitmapAlphaMode.Premultiplied);
-
-                   SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
-                   await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
-                   imgNewReceipt.Source = bitmapSource;
-             **/
-
-
-            CameraCaptureUI dialog = new CameraCaptureUI();
-            Size aspectRatio = new Size(16, 9);
-            dialog.PhotoSettings.CroppedAspectRatio = aspectRatio;
-
-            StorageFile file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
-
-            testPic.Source = file;
-
-
-
+        public void acceptEntry()
+        {
 
         }
 
+        /// <summary>
+        /// This method used to take a picture from the webcam, and save it to a picturebox.
+        /// </summary>
+        public async void takePicture()
+        {
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            captureUI.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
+
+            StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (photo == null)
+            {
+                // User cancelled photo capture
+                System.Diagnostics.Debug.WriteLine("photo = null");
+                return;
+            }
+
+            IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+            SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+            SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(softwareBitmap,
+            BitmapPixelFormat.Bgra8,
+            BitmapAlphaMode.Premultiplied);
+
+            SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+            await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
+            imgNewReceipt.Source = bitmapSource;
+        }
+
+        public void dragStart(object sender, PointerRoutedEventArgs e)
+        {
+            Debug.WriteLine("dragStart");
+            dragging = true;
+            
+            dragStartPos[0] = Convert.ToInt32(e.GetCurrentPoint(imgNewReceipt).Position.X);
+            dragStartPos[1] = Convert.ToInt32(e.GetCurrentPoint(imgNewReceipt).Position.Y);
+
+            dragFinishPos[0] = dragStartPos[0];
+            dragFinishPos[1] = dragStartPos[1];
+
+            rect.Visibility = Visibility.Visible;
+
+            Debug.WriteLine("dragStartPost: " + dragStartPos[0].ToString() + ", " +dragStartPos[1].ToString());
+        }
+
+        public void dragFinish(object sender, PointerRoutedEventArgs e)
+        {
+            dragging = false;
+            Debug.WriteLine("dragFinish");
+
+            dragFinishPos[0] = Convert.ToInt32(e.GetCurrentPoint(imgNewReceipt).Position.X);
+            dragFinishPos[1] = Convert.ToInt32(e.GetCurrentPoint(imgNewReceipt).Position.Y);
+
+            Debug.WriteLine("dragFinishPos: " + dragFinishPos[0].ToString() + ", " + dragFinishPos[1].ToString());
+
+        }
+
+        public void dragMove(object sender, PointerRoutedEventArgs e)
+        {
+            if (dragging)
+            {
+                //Debug.WriteLine("drag move");
+                dragFinishPos[0] = Convert.ToInt32(e.GetCurrentPoint(imgNewReceipt).Position.X);
+                dragFinishPos[1] = Convert.ToInt32(e.GetCurrentPoint(imgNewReceipt).Position.Y);
+
+                CompositionTarget_Rendering(sender, null);
+            }
+
+        }
+
+        public void homeClicked()
+        {
+
+        }
+
+        public void spendingsClicked()
+        {
+
+        }
+
+        public void VATClicked()
+        {
+
+        }
     }
 }
